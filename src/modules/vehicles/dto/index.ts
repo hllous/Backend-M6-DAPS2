73af3677b8e3a21186cbbb81fe7228a8c296b283
Benjamin Martinez <benjanmartinez@gmail.com>
2026-09-01 12:46:00 -0300

    feat(vehicles): add CRUD controller, service and DTOs with Swagger docs
    - VehiclesController with 5 CRUD endpoints and exhaustive Swagger
    
    - VehiclesService with Prisma CRUD, soft-delete, Decimal-to-number mapping
    
    - DTOs: CreateVehicleDto, UpdateVehicleDto, VehicleResponseDto, QueryVehiclesDto
    
    - vehicleType immutable after creation; plate unique constraint handled
    
    Refs #001

---
+++ src/modules/vehicles/dto/index.ts
@@ -0,0 +1,4 @@
+export { CreateVehicleDto } from './create-vehicle.dto';
+export { UpdateVehicleDto } from './update-vehicle.dto';
+export { VehicleResponseDto } from './vehicle-response.dto';
+export { QueryVehiclesDto } from './query-vehicles.dto';
